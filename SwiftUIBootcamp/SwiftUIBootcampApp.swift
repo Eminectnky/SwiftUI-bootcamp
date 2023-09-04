03c710830077b369e4083560c6a24a0d3622d0ab
Emine <eminecetinkayaa98@gmail.com>
2023-09-04 14:23:49 +0300

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Emine CETINKAYA on 4.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
