7993cc338f05b8bfb1b0e42a5ac14d3c427ac6c4
Emine Çetinkaya <eminecetinkayaa98@gmail.com>
2023-11-12 15:38:36 +0300

    Adding Animations in SwiftUI

---
+++ SwiftUIBootcamp/AnimationBootcamp.swift
@@ -0,0 +1,38 @@
+//
+//  AnimationBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Emine CETINKAYA on 12.11.2023.
+//
+
+import SwiftUI
+
+struct AnimationBootcamp: View {
+    
+    @State var isAnimated: Bool = false
+    var body: some View {
+        VStack{
+            
+            Button("Button") {
+                    isAnimated.toggle()
+                }
+                
+            }
+            Spacer()
+            RoundedRectangle(cornerRadius: isAnimated ? 50 : 25)
+                .fill(isAnimated ? Color.red : Color.green)
+                .frame(
+                    width: isAnimated ? 100 : 300,
+                    height: isAnimated ? 100 : 300)
+                .rotationEffect(Angle(degrees: isAnimated ? 360 : 0))
+                .offset(y: isAnimated ? 300 : 0)
+                .animation(Animation.default.repeatForever(autoreverses: true))
+            
+            Spacer()
+        }
+    }
+
+
+#Preview {
+    AnimationBootcamp()
+}
