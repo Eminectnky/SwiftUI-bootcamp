a1055b5d3771fc0471fa4d95730b61ab23042608
Emine Çetinkaya <eminecetinkayaa98@gmail.com>
2024-01-07 23:40:25 +0300

    Picker and PickerStyles in SwiftUI

---
+++ SwiftUIBootcamp/PickerBootcamp.swift
@@ -0,0 +1,106 @@
+//
+//  PickerBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Emine CETINKAYA on 6.01.2024.
+//
+
+import SwiftUI
+
+struct PickerBootcamp: View {
+    
+    @State var selection: String = "Most Recent"
+    let filterOptions: [String] = [
+        "Most Recent", "Most Popular", "Most Liked"
+    ]
+    
+    init() {
+        UISegmentedControl.appearance().selectedSegmentTintColor = UIColor.red
+        
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor : UIColor.white
+        ]
+        
+        
+        UISegmentedControl.appearance().setTitleTextAttributes(attributes, for: .selected)
+    }
+    
+    
+    
+    var body: some View {
+        
+        Picker(selection: $selection,
+               label: Text("Picker"),
+               content: {
+            ForEach(filterOptions.indices) { index in
+                Text(filterOptions[index])
+                    .tag(filterOptions[index])
+            }
+            
+        })
+        
+        .pickerStyle(SegmentedPickerStyle())
+//        .background(Color.red)
+        
+        
+        
+//
+//        Picker(
+//            selection: $selection,
+//            
+//            label:
+//                HStack {
+//                    Text("Filter:")
+//                    Text(selection)
+//                }
+//                .font(.headline)
+//                .foregroundColor(.white)
+//                .padding()
+//                .padding(.horizontal)
+//                .background(Color.blue)
+//                .cornerRadius(10)
+//                .shadow(color: Color.blue.opacity(0.3), radius: 10, x:0, y:10)
+//            ,
+//            content: {
+//                ForEach(filterOptions, id: \.self) { option in
+//                    HStack {
+//                        Text(option)
+//                        Image(systemName: "heart.fill")
+//                    }
+//                    .tag(option)
+//
+//                }
+//        })
+//        
+//        .pickerStyle(MenuPickerStyle())
+        
+//        VStack{
+//            
+//            HStack{
+//                Text("Age:")
+//                Text(selection)
+//            }
+//            Picker(
+//                selection: $selection,
+//                label: 
+//                    Text("Picker"),
+//                content:  {
+//                    ForEach(18..<100) {
+//                        number in
+//                        Text("\(number)")
+//                            .font(.headline)
+//                            .foregroundColor(.red)
+//                            .tag("\(number)")
+//                    }
+//                })
+//            .pickerStyle(WheelPickerStyle())
+            //            .background(Color.gray.opacity(0.3))
+//        }
+      
+               
+    }
+}
+
+#Preview {
+    PickerBootcamp()
+}
