8aa8f99701475f5beb9e635349a2a71b5bf21e71
Emine Çetinkaya <eminecetinkayaa98@gmail.com>
2024-01-11 16:01:15 +0300

    Adapt for Dark Mode in SwiftUI project

---
+++ SwiftUIBootcamp/DarkModeBootcamp.swift
@@ -0,0 +1,49 @@
+//
+//  DarkModeBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Emine CETINKAYA on 11.01.2024.
+//
+
+import SwiftUI
+
+struct DarkModeBootcamp: View {
+
+    @Environment(\.colorScheme) var colorScheme
+    
+    var body: some View {
+        NavigationView{
+            ScrollView{
+                VStack(spacing: 20) {
+                    
+                    Text("This color is PRIMARY")
+                        .foregroundColor(.primary)
+                    Text("This color is SECONDARY")
+                        .foregroundColor(.secondary)
+                    Text("This color is BLACK")
+                        .foregroundColor(.black)
+                    Text("This color is WHITE")
+                        .foregroundColor(.white)
+                    Text("This color is RED")
+                        .foregroundColor(.red)
+                    Text("This color is globally adaptive!")
+                        .foregroundColor(Color("AdaptiveColor"))
+                    Text("This color is locally adaptive!")
+                        .foregroundColor(colorScheme == .light ? .green : .yellow)
+                    
+               }
+            }
+            .navigationTitle("Dark Mode Bootcamp")
+        }
+      
+    }
+}
+
+#Preview {
+    
+    Group{
+        DarkModeBootcamp()
+            .preferredColorScheme(.light)
+    }
+  
+}
