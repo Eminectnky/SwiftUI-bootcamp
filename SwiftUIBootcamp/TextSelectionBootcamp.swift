a19620b827099eb2f95303f4799fddf0682b7771
Emine Çetinkaya <eminecetinkayaa98@gmail.com>
2024-01-21 19:26:39 +0300

    How to select text with TextSelection in SwiftUI

---
+++ SwiftUIBootcamp/TextSelectionBootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelectionBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Emine CETINKAYA on 21.01.2024.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled) // Used to add copy and share options to text
+    }
+}
+
+#Preview {
+    TextSelectionBootcamp()
+}
