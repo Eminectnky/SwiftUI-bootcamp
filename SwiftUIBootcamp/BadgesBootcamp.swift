a1e12bc5c49fcd0c251759307a1d6f5628f55f31
Emine Çetinkaya <eminecetinkayaa98@gmail.com>
2024-01-22 12:04:40 +0300

    How to add Badges to SwiftUI TabView and List in iOS 15

---
+++ SwiftUIBootcamp/BadgesBootcamp.swift
@@ -0,0 +1,50 @@
+//
+//  BadgesBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Emine CETINKAYA on 22.01.2024.
+//
+
+import SwiftUI
+
+//List
+//TabView
+
+struct BadgesBootcamp: View {
+    var body: some View {
+        
+        
+        List{
+            Text("Hello World!")
+                .badge("NEW ITEMS!")
+            Text("Hello World")
+                .badge(5)
+            Text("Hello World")
+            Text("Hello World")
+        }
+//        TabView {
+//            Color.red
+//                .tabItem {
+//                    Image(systemName: "heart.fill")
+//                    Text("Hello")
+//                }
+//                .badge("NEW")
+//            
+//            Color.green
+//                .tabItem {
+//                    Image(systemName: "heart.fill")
+//                    Text("Hello")
+//                }
+//            
+//            Color.blue
+//                .tabItem {
+//                    Image(systemName: "heart.fill")
+//                    Text("Hello")
+//                }
+//        }
+    }
+}
+
+#Preview {
+    BadgesBootcamp()
+}
